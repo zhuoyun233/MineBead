2dead8ae61d0eb6ee351d4926244ef71a091324c
zhuoyun233 <zhuoyun233@gmail.com>
2025-12-25 00:20:02 +0800

    First Commit

---
+++ common/src/main/java/gg/bluechair/minebead/ExampleMod.java
@@ -0,0 +1,9 @@
+package gg.bluechair.minebead;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "minebead";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric-like/src/main/java/gg/bluechair/minebead/fabriclike/ExampleModFabricLike.java
@@ -0,0 +1,10 @@
+package gg.bluechair.minebead.fabriclike;
+
+import gg.bluechair.minebead.ExampleMod;
+
+public final class ExampleModFabricLike {
+    public static void init() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
