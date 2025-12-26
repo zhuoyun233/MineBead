f0e502168c5077eedb9232e403bd06348d035d15
zhuoyun233 <zhuoyun233@gmail.com>
2025-12-26 19:24:40 +0800

    创建了color_block

--- common/src/main/java/gg/bluechair/minebead/ExampleMod.java
+++ common/src/main/java/gg/bluechair/minebead/ExampleMod.java
@@ -1,9 +1,15 @@
 package gg.bluechair.minebead;
+import registry.ModBlockEntities;
+import registry.ModBlocks;
+import registry.ModItems;
 
 public final class ExampleMod {
     public static final String MOD_ID = "minebead";
 
     public static void init() {
         // Write common init code here.
+        ModBlocks.register();
+        ModItems.register();
+        ModBlockEntities.register();
     }
 }
